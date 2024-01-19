2efe79c895850f8344be83cc86bce00933795fac
Vikki9121 <vignesh24csengineer@gmail.com>
2024-01-19 17:40:08 +0530

    header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -6,8 +6,9 @@ const Header = () => {
   return (
     <header className="fixed top-0 w-full z-50 bg-blue-600 text-white text-xl p-4">
       <div className="container flex justify-center items-center h-full">
-        <div className="flex">
+        <div className="flex items-center">
           <Image src={logo} alt="Logo" height={32} width={32} />
+          <div className="ml-2 font-semibold">GCP Arcade Helper</div>
         </div>
       </div>
     </header>
